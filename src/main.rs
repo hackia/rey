65ed30940042cd57a1409bee0b5ee1b01e477b3a
hackia <otechdoia@gmail.com>
2025-09-06 19:19:56 +0200

    update rey

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
+use clap::{ArgMatches, Command};
+use rey::console::Console;
+
 pub mod console;
+
+fn rey() -> ArgMatches {
+    Command::new("rey")
+        .version("0.1.0")
+        .author("Seido <seido@hackiado.com>")
+        .about("about")
+        .subcommand(Command::new("clean").about("Clean the project"))
+        .subcommand(Command::new("init").about("Initialize the project"))
+        .subcommand(Command::new("serve").about("Serve the project"))
+        .subcommand(Command::new("watch").about("Watch the project"))
+        .get_matches()
+}
 fn main() {
-    println!("Hello, world!");
+    let matches = rey();
+    match matches.subcommand() {
+        Some(("clean", _sub_m)) => {
+            Console::clean();
+        }
+        Some(("init", _)) => {
+            Console::init();
+        }
+        Some(("serve", _)) => {
+            Console::serve();
+        }
+        Some(("watch", _)) => {
+            Console::watch();
+        }
+        _ => {
+            println!("No valid subcommand was used. Use --help for more information.");
+        }
+    }
 }
